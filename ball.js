8f9a2385057dac8ccf9e9110d874d363311428b2
unknown <alejandro.hoyo@alu.uclm.es>
2025-11-27 18:16:14 +0100

    Mi pelota linea 50

--- ball.js
+++ ball.js
@@ -47,7 +47,7 @@ class Ball {
     }
 
     updateBallMovement() {
-        // Movemen  
+        // Movem 
         this.position.add(this.velocity);
         // Check border
         if (this.position.y > height - this.radio || this.position.y < 0 + this.radio) {
