622023f36ad768f81a7b51436f1414663ea2eda5
unknown <alejandro.hoyo@alu.uclm.es>
2025-11-27 17:59:31 +0100

    Cambio

--- ball.js
+++ ball.js
@@ -47,7 +47,7 @@ class Ball {
     }
 
     updateBallMovement() {
-        // Movement of the ball
+        // Movement of 
         this.position.add(this.velocity);
         // Check borders
         if (this.position.y > height - this.radio || this.position.y < 0 + this.radio) {
