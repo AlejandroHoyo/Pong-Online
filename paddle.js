ac0e2f240deb24e43666d76064e64330e2a08b2e
unknown <alejandro.hoyo@alu.uclm.es>
2025-12-03 21:16:58 +0100

    Cambios 3 diciembre

--- paddle.js
+++ paddle.js
@@ -23,7 +23,6 @@ class Paddle {
 
     checkBorders(maximumHeight) { // It is better to check each case in a separate way 
 
-        // Stop movement if the paddle goes out of bo
         if (this.position.y < 0) {
             this.position.y = 0;
             this.speed = 0;
